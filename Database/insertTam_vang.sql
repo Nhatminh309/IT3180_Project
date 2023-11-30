24abd034e5cc890fa337ee99b226aeb8e7e870e5
Nhatminh309 <147911728+Nhatminh309@users.noreply.github.com>
2023-12-01 02:48:44 +0700

    Insert table Tam_vang

---
+++ Database/insertTam_vang.sql
@@ -0,0 +1,13 @@
+-- Thêm 10 dòng dữ liệu vào bảng Tam_vang
+INSERT INTO Tam_vang (ID, Ma_nhan_khau, Ngay_tam_vang, Noi_den, Da_xac_nhan)
+VALUES 
+  (1, 1, '2023-01-01', 'Thành phố A', TRUE),
+  (2, 2, '2023-02-01', 'Quận B', TRUE),
+  (3, 3, '2023-03-01', 'Huyện C', TRUE),
+  (4, 4, '2023-04-01', 'Xã D', TRUE),
+  (5, 5, '2023-05-01', 'Thị trấn E', FALSE),
+  (6, 6, '2023-06-01', 'Thành phố F', FALSE),
+  (7, 7, '2023-07-01', 'Quận G', TRUE),
+  (8, 8, '2023-08-01', 'Huyện H', TRUE),
+  (9, 9, '2023-09-01', 'Xã I', TRUE),
+  (10, 10, '2023-10-01', 'Thị trấn J', TRUE);
