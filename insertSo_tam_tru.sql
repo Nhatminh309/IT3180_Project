acfb1c26457d470dc783cc51afc9497a79833623
Nhatminh309 <147911728+Nhatminh309@users.noreply.github.com>
2023-12-01 08:59:13 +0700

    insert Tam_tru

---
+++ insertSo_tam_tru.sql
@@ -0,0 +1,24 @@
+INSERT INTO So_tam_tru (ID, Dia_chi_thuong_tru, Ngay_dang_ky, Thoi_han, Ma_nhan_khau, Da_xac_nhan)
+VALUES
+  (1, '123 Đường ABC, Quận 1, Thành phố Hồ Chí Minh', '2023-01-01', '2023-06-30', 101, TRUE),
+  (2, '456 Đường XYZ, Quận 2, Thành phố Hồ Chí Minh', '2023-02-15', '2023-08-15', 102, FALSE),
+  (3, '789 Đường LMN, Quận 3, Thành phố Hồ Chí Minh', '2023-03-20', '2023-09-20', 103, TRUE),
+  (4, '101 Đường PQR, Quận 4, Thành phố Hồ Chí Minh', '2023-04-10', '2023-10-10', 104, FALSE),
+  (5, '202 Đường UVW, Quận 5, Thành phố Hồ Chí Minh', '2023-05-05', '2023-11-05', 105, TRUE);
+INSERT INTO So_tam_tru (ID, Dia_chi_thuong_tru, Ngay_dang_ky, Thoi_han, Ma_nhan_khau, Da_xac_nhan)
+VALUES
+  (6, '303 Đường EFG, Quận 6, Thành phố Hồ Chí Minh', '2023-06-15', '2023-12-15', 106, FALSE),
+  (7, '404 Đường HIJ, Quận 7, Thành phố Hồ Chí Minh', '2023-07-05', '2024-01-05', 107, TRUE),
+  (8, '505 Đường KLM, Quận 8, Thành phố Hồ Chí Minh', '2023-08-20', '2024-02-20', 108, FALSE),
+  (9, '606 Đường NOP, Quận 9, Thành phố Hồ Chí Minh', '2023-09-10', '2024-03-10', 109, TRUE),
+  (10, '707 Đường QRS, Quận 10, Thành phố Hồ Chí Minh', '2023-10-25', '2024-04-25', 110, FALSE),
+  (11, '808 Đường TUV, Quận 11, Thành phố Hồ Chí Minh', '2023-11-15', '2024-05-15', 111, TRUE),
+  (12, '909 Đường WXY, Quận 12, Thành phố Hồ Chí Minh', '2023-12-01', '2024-06-01', 112, FALSE),
+  (13, '111 Đường ABC, Quận 1, Thành phố Hồ Chí Minh', '2024-01-10', '2024-07-10', 113, TRUE),
+  (14, '222 Đường XYZ, Quận 2, Thành phố Hồ Chí Minh', '2024-02-28', '2024-08-28', 114, FALSE),
+  (15, '333 Đường LMN, Quận 3, Thành phố Hồ Chí Minh', '2024-03-25', '2024-09-25', 115, TRUE),
+  (16, '444 Đường PQR, Quận 4, Thành phố Hồ Chí Minh', '2024-04-15', '2024-10-15', 116, FALSE),
+  (17, '555 Đường UVW, Quận 5, Thành phố Hồ Chí Minh', '2024-05-10', '2024-11-10', 117, TRUE),
+  (18, '666 Đường EFG, Quận 6, Thành phố Hồ Chí Minh', '2024-06-20', '2024-12-20', 118, FALSE),
+  (19, '777 Đường HIJ, Quận 7, Thành phố Hồ Chí Minh', '2024-07-08', '2025-01-08', 119, TRUE),
+  (20, '888 Đường KLM, Quận 8, Thành phố Hồ Chí Minh', '2024-08-18', '2025-02-18', 120, FALSE);
