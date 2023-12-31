7088710f46c5e50a23c97d46ebd9aa256ce2302f
dvmduc5269 <ducduvumanh@gmail.com>
2024-01-01 01:31:29 +0700

    h

--- QuanLyDanCu/src/giaodien/GiaoDienBanDau.java
+++ QuanLyDanCu/src/giaodien/GiaoDienBanDau.java
@@ -11,18 +11,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
-public class GiaoDien {
+public class GiaoDienBanDau extends GiaoDien {
     private static JFrame frame;
+    private static JButton dangNhap;
+    private static JButton dangKy;
+    private static JButton btnHome;
+    private static JButton btnAbout;
+    private static JLabel toolBarLabel = new JLabel("CỔNG THÔNG TIN THỦ TỤC HÀNH CHÍNH");
     private static JPanel verticalBar;
     private static JPanel horizontalBar;
-
-    public GiaoDien() {
+    public GiaoDienBanDau() {
         frame = new JFrame("Quản lý dân cư");
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(true);
-        frame.setMinimumSize(new Dimension(600, 400));
+        frame.setResizable(true)    ;
+
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
 
@@ -44,27 +48,47 @@ public class GiaoDien {
         horizontalBar.setBackground(Color.decode("#004AAD"));
         horizontalBar.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight() / 6)); // Chiều rộng full, chiều cao 15%
 
-        frame.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                adjustComponentsSize();
-            }
-        });
 
+        dangNhap = new JButton("Đăng nhập");
+        dangNhap.setBackground(Color.decode("#38B6FF"));
+        dangNhap.setForeground(Color.WHITE);
+        dangNhap.setFont(new Font("Arial", Font.PLAIN, 20));
+        dangNhap.setBorder(BorderFactory.createEmptyBorder(5, 15, 5, 15)); // Thiết lập border
+        dangNhap.setFocusPainted(false); // Loại bỏ viền focus
+        
+        dangKy = new JButton("Đăng ký");
+        dangKy.setBackground(Color.decode("#38B6FF"));
+        dangKy.setForeground(Color.WHITE);
+        dangKy.setFont(new Font("Arial", Font.PLAIN, 20));
+        dangKy.setBorder(BorderFactory.createEmptyBorder(5, 15, 5, 15)); // Thiết lập border
+        dangKy.setFocusPainted(false); // Loại bỏ viền focus
+
+
+        horizontalBar.add(Box.createHorizontalGlue()); // Thêm glue đưa các thành phần về bên phải
+        horizontalBar.add(dangKy);
+        horizontalBar.add(Box.createRigidArea(new Dimension(10, 0))); // Để tạo khoảng cách giữa nút
+        horizontalBar.add(dangNhap);
+
+        btnHome = new JButton("Home");
+        btnAbout = new JButton("About");
+        verticalBar.setLayout(new BoxLayout(verticalBar, BoxLayout.Y_AXIS));
+        verticalBar.add(btnHome);
+        verticalBar.add(btnAbout);
 
         mainPanel.add(verticalBar, BorderLayout.WEST);
         mainPanel.add(horizontalBar, BorderLayout.NORTH);
 
+        frame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                verticalBar.setPreferredSize(new Dimension(frame.getWidth() / 6, frame.getHeight()));
+                horizontalBar.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight() / 6));
+                frame.revalidate();
+            }
+        });
         frame.add(mainPanel);
         frame.setVisible(true);
     }
-    private void adjustComponentsSize() {
-        verticalBar.setPreferredSize(new Dimension(frame.getWidth() / 6, frame.getHeight()));
-        horizontalBar.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight() / 6));
-        frame.revalidate();
-    }
-
-
     public String getStringURL() {
         String URL = "jdbc:postgresql://localhost:5432/postgres";
         return URL;
@@ -77,6 +101,6 @@ public class GiaoDien {
 
 
     public static void main(String[] args) {
-        new GiaoDien();
+        new GiaoDienBanDau();
     }
 }
