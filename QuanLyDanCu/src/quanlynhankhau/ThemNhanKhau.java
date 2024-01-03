d0e15de9f7de63af522f10bb11664c1883531ddd
longmvp27 <longhoangle999@gmail.com>
2024-01-04 05:08:33 +0700

    update them nhan khau

--- QuanLyDanCu/src/quanlynhankhau/ThemNhanKhau.java
+++ QuanLyDanCu/src/quanlynhankhau/ThemNhanKhau.java
@@ -50,7 +50,7 @@ public class ThemNhanKhau extends ConnectDatabase {
     private static JButton addButton;
     private static JLabel warnLabel;
     public ThemNhanKhau() {
-        frame = new JFrame("Đăng ký");
+        frame = new JFrame("Thêm nhân khẩu");
         frame.setSize(1000, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
