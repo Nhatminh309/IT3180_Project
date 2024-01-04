525e28b11c4940bed3e55cf09d63b6c654767c9b
Nhatminh309 <nhatminh2k22k3@gmail.com>
2024-01-04 22:19:44 +0700

    done quan ly

--- QuanLyDanCu/src/quanlyhokhau/ThemHoKhau.java
+++ QuanLyDanCu/src/quanlyhokhau/ThemHoKhau.java
@@ -1,18 +1,16 @@
 package quanlydancu.src.quanlyhokhau;
 
-import quanlydancu.src.giaodien.GiaoDienChung;
+import quanlydancu.src.giaodien.*;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
 
 import static com.sun.glass.ui.Cursor.setVisible;
 
@@ -26,6 +24,7 @@ public class ThemHoKhau extends GiaoDienChung {
     private JTextField txtSoLuongXeMay;
     private JTextField txtSoLuongOTo;
     private JTextField txtDienTich;
+    private JComboBox<String> cboDiaDiem;
     private JButton btnThemHoKhau;
 
     public ThemHoKhau() {
@@ -39,10 +38,11 @@ public class ThemHoKhau extends GiaoDienChung {
         txtSoLuongXeMay = new JTextField();
         txtSoLuongOTo = new JTextField();
         txtDienTich = new JTextField();
+        cboDiaDiem = new JComboBox<>(new String[]{"Chung cư BlueMoon", "Tổ dân phố 7"});
         btnThemHoKhau = new JButton("Thêm hộ khẩu");
 
         JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridLayout(9, 2));
+        inputPanel.setLayout(new GridLayout(10, 2));
         inputPanel.add(createLabel("Số nhà:"));
         inputPanel.add(txtSoNha);
         inputPanel.add(createLabel("Tên đường:"));
@@ -53,6 +53,8 @@ public class ThemHoKhau extends GiaoDienChung {
         inputPanel.add(txtTenQuan);
         inputPanel.add(createLabel("Tên thành phố:"));
         inputPanel.add(txtTenThanhPho);
+        inputPanel.add(createLabel("Địa điểm:"));
+        inputPanel.add(cboDiaDiem);
         inputPanel.add(createLabel("Số lượng xe máy:"));
         inputPanel.add(txtSoLuongXeMay);
         inputPanel.add(createLabel("Số lượng ô tô:"));
@@ -68,16 +70,19 @@ public class ThemHoKhau extends GiaoDienChung {
                 int confirmResult = JOptionPane.showConfirmDialog(frame, "Bạn có chắc chắn muốn thêm hộ khẩu không?", "Xác nhận", JOptionPane.YES_NO_OPTION);
 
                 if (confirmResult == JOptionPane.YES_OPTION) {
+                    // Lấy giá trị từ các trường nhập liệu
                     String soNha = txtSoNha.getText();
                     String tenDuong = txtTenDuong.getText();
                     String tenPhuong = txtTenPhuong.getText();
                     String tenQuan = txtTenQuan.getText();
                     String tenThanhPho = txtTenThanhPho.getText();
+                    String diaDiem = (String) cboDiaDiem.getSelectedItem();
                     String soLuongXeMay = txtSoLuongXeMay.getText();
                     String soLuongOTo = txtSoLuongOTo.getText();
                     String dienTich = txtDienTich.getText();
 
-                    if (soNha.isEmpty() || tenDuong.isEmpty() || tenPhuong.isEmpty() || tenQuan.isEmpty() || tenThanhPho.isEmpty() || soLuongXeMay.isEmpty() || soLuongOTo.isEmpty() || dienTich.isEmpty()) {
+                    // Kiểm tra xem có trường nào trống không
+                    if (soNha.isEmpty() || tenDuong.isEmpty() || tenPhuong.isEmpty() || tenQuan.isEmpty() || tenThanhPho.isEmpty() || diaDiem.isEmpty() || soLuongXeMay.isEmpty() || soLuongOTo.isEmpty() || dienTich.isEmpty()) {
                         JOptionPane.showMessageDialog(frame, "Vui lòng nhập đầy đủ thông tin.");
                         return;
                     }
@@ -92,37 +97,33 @@ public class ThemHoKhau extends GiaoDienChung {
                     }
 
                     try (
-                            // Truy vấn cơ sở dữ liệu để lấy giá trị ID hiện tại
                             Statement statement = connection.createStatement();
                             ResultSet resultSet = statement.executeQuery("SELECT MAX(Ma_ho_khau) FROM ho_khau");
-                            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO ho_khau (Ma_ho_khau, So_nha, Ten_duong, Ten_phuong, Ten_quan, Ten_thanh_pho, Da_xac_nhan, So_luong_xe_may, So_luong_o_to, Dien_tich) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)
+                            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO ho_khau (Ma_ho_khau, Dia_diem, So_nha, Ten_duong, Ten_phuong, Ten_quan, Ten_thanh_pho, Da_xac_nhan, So_luong_xe_may, So_luong_o_to, Dien_tich) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)
                     ) {
                         int currentMaxId = 0;
 
-                        // Lấy giá trị ID hiện tại
                         if (resultSet.next()) {
                             currentMaxId = resultSet.getInt(1);
                         }
 
-                        // Tăng giá trị ID lấy được thêm 1
                         int yourSpecificId = currentMaxId + 1;
 
-                        // Truyền giá trị ID cụ thể vào câu lệnh SQL
                         preparedStatement.setInt(1, yourSpecificId);
-                        preparedStatement.setString(2, soNha);
-                        preparedStatement.setString(3, tenDuong);
-                        preparedStatement.setString(4, tenPhuong);
-                        preparedStatement.setString(5, tenQuan);
-                        preparedStatement.setString(6, tenThanhPho);
-                        preparedStatement.setBoolean(7, true);
-                        preparedStatement.setInt(8, Integer.parseInt(soLuongXeMay));
-                        preparedStatement.setInt(9, Integer.parseInt(soLuongOTo));
-                        preparedStatement.setInt(10, Integer.parseInt(dienTich));
+                        preparedStatement.setString(2, diaDiem);
+                        preparedStatement.setString(3, soNha);
+                        preparedStatement.setString(4, tenDuong);
+                        preparedStatement.setString(5, tenPhuong);
+                        preparedStatement.setString(6, tenQuan);
+                        preparedStatement.setString(7, tenThanhPho);
+                        preparedStatement.setBoolean(8, true);
+                        preparedStatement.setInt(9, Integer.parseInt(soLuongXeMay));
+                        preparedStatement.setInt(10, Integer.parseInt(soLuongOTo));
+                        preparedStatement.setInt(11, Integer.parseInt(dienTich));
 
                         int affectedRows = preparedStatement.executeUpdate();
 
                         if (affectedRows > 0) {
-                            // Retrieve the automatically generated ID
                             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
                             if (generatedKeys.next()) {
                                 int generatedId = generatedKeys.getInt(1);
@@ -137,6 +138,7 @@ public class ThemHoKhau extends GiaoDienChung {
                             txtTenPhuong.setText("");
                             txtTenQuan.setText("");
                             txtTenThanhPho.setText("");
+                            cboDiaDiem.setSelectedIndex(0);
                             txtSoLuongXeMay.setText("");
                             txtSoLuongOTo.setText("");
                             txtDienTich.setText("");
@@ -154,6 +156,7 @@ public class ThemHoKhau extends GiaoDienChung {
         rightPanel.add(inputPanel, BorderLayout.CENTER);
 
         frame.setVisible(true);
+
         // Add "Quay về" button
         JButton btnQuayVe = new JButton("Quay về");
         btnQuayVe.addActionListener(new ActionListener() {
@@ -162,6 +165,7 @@ public class ThemHoKhau extends GiaoDienChung {
                 quayVeQuanLyHoKhau();
             }
         });
+
         // Add the "Quay về" button to the rightPanel
         rightPanel.add(btnQuayVe, BorderLayout.SOUTH);
 
@@ -169,13 +173,12 @@ public class ThemHoKhau extends GiaoDienChung {
     }
 
     private void quayVeQuanLyHoKhau() {
-        // Tạo đối tượng QuanLyHoKhau và hiển thị nó
         QuanLyHoKhau quanLyHoKhau = new QuanLyHoKhau();
         showFrame();
-        frame.dispose(); // Đóng frame hiện tại nếu cần
+        frame.dispose();
     }
+
     public void showFrame() {
-        // Make the frame visible
         setVisible(true);
     }
 
@@ -185,7 +188,4 @@ public class ThemHoKhau extends GiaoDienChung {
         label.setForeground(Color.BLACK);
         return label;
     }
-
-
-
 }
